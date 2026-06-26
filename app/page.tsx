262625b126b62bbfc9146b257c44ee642b8a49e4
choplagos <70777342+choplagos@users.noreply.github.com>
2026-06-26 15:45:03 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic'
 import React, { useState, useEffect } from 'react'
 import Navbar from '@/components/Navbar'
 import Hero from '@/components/Hero'
